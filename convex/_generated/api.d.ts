d02b84b6812f254274a6e07684bcdc59c36088cb
crithcraft <crithcraft@lukevium.ru>
2023-11-09 20:11:48 +0500

    - restore shema

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as documents from "../documents";
+import type * as shema from "../shema";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,7 +25,7 @@ import type * as documents from "../documents";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  documents: typeof documents;
+  shema: typeof shema;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
